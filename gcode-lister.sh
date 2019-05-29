ec3cf262ea787c7dc2b10ade0f66294dc2900906
pringon <gojedan98@yahoo.com>
2019-05-29 17:35:05 +0100

    Refactor some scripts

--- gcode-lister.sh
+++ gcode-lister.sh
@@ -1,29 +1,43 @@
+#!/bin/bash
+extensions=(
+  ["typescript"]="ts"
+  ["javascript"]="js"
+  ["python"]="py"
+  ["c++"]="cpp"
+  ["c"]="c"
+  ["haskell"]="hs"
+)
+
 function escape_underscore {
   string=$1
-  echo ${string/_/\\_}
+  echo "${string/_/\\_}"
 }
 
 function begin_listing {
-  echo "\\textbf{$(escape_underscore $1)}" >> listing.tex
-  echo "\\begin{minted}" >> listing.tex
-  echo "[" >> listing.tex
-  echo "linenos" >> listing.tex
-  echo "]" >> listing.tex
-  echo "{$2}" >> listing.tex
-  echo "" >> listing.tex
+  {
+    echo "\\textbf{$(escape_underscore $1)}"
+    "\\begin{minted}"
+    "["
+    "linenos"
+    "]"
+    "{$2}"
+    ""
+  } >> listing.tex
 }
 
 function end_listing {
-  echo "\\end{minted}" >> listing.tex
-  echo "" >> listing.tex
+  {
+    echo "\\end{minted}"
+    ""
+  } >> listing.tex
 }
 
 rm -f listing.tex
 touch listing.tex
 
-for file in $(find ./$1 -type f -name "*" ! -name "config.*" ! -name ".*");
+for file in $(find ./$1 -type f -name "*.${extensions[$2]}");
 do
-  begin_listing $file $2
-  cat $file >> listing.tex
+  begin_listing "$file" "$2"
+  cat "$file" >> listing.tex
   end_listing
 done
